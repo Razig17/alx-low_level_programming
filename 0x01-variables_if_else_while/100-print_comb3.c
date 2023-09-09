53cc12140d8359ebaa324e19068416618a98981e
Razig <razig@soleb.com>
2023-09-08 19:05:32 -0700

    0x01-variables_if_else_while c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(j = i + 1 ; j < 10; j++)
+		{
+			putchar('0' +  i);
+			putchar('0' +  j);
+			if (i == 8)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
