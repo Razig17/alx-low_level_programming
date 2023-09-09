da2c76c0f0595992becd85136283c6204716ab69
Razig <razig@soleb.com>
2023-09-08 17:21:59 -0700

    0x01-variables_if_else_while c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**                                                                                
+ * main - prints the alphabet in lowercase and uppercase                           
+ * Return: 0                                                                       
+*/
+int main(void)
+{
+        char c ;
+
+        for (c = 'a'; c != 'z'; c++)
+                putchar(c);
+        for (c = 'A'; c != 'Z'; c++)
+                putchar(c);
+        putcah(\n);
+        return 0;
+}
