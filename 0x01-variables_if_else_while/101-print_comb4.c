cb00ccc6587cf30f87042c5e16e814a612d6ebac
Razig <razig@soleb.com>
2023-09-08 19:16:33 -0700

    0x01-variables_if_else_while c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+for (i = 0; i < 8; i++)
+{
+	for (j = i+1; j < 10; j++)
+	{
+		for (k = j + 1; k < 10; k++)
+		{
+			putchar('0' +  i);
+			putchar('0' +  j);
+			putchar('0' +  k);
+			if (i == 7)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}
