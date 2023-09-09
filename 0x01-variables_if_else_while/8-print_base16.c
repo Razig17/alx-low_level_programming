d84eaba4bd51ff7ef67d6eeed51b3e2adb6e14b7
Razig <razig@soleb.com>
2023-09-08 18:31:39 -0700

    0x01-variables_if_else_while c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers of base  from 0 to f
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' +  n);
+	for (c = 'a'; c < 'g'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
