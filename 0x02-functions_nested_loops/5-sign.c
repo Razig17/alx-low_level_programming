04e1020b4e4afb0e80a431e2b85abc962b3aa19a
Razig <razig@soleb.com>
2023-09-12 16:37:49 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - checks for alphabetic character
+ *
+ *@n: checks input of fucnction
+ *
+ * Return: returns 1 if c is the number is postive 0 if its zero and -1 if it is negative
+*/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		return (0);
+	}	
+}
