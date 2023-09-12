9a3430e3f356874ff76666944759c932985d5ac2
Razig <razig@soleb.com>
2023-09-12 16:57:47 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ *@: checks input of fucnction
+ *
+ * Return: returns the absolute value of a number
+*/
+int _abs(int)
+{
+	int a;
+
+
+	if (a < 0)
+		return (a * -1);
+	else
+		return (a)
+}
