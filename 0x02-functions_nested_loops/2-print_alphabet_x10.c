d13d88524a8f20f73bb560f15489bd6dd0075184
Razig <razig@soleb.com>
2023-09-12 15:35:09 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * prints alphabets using _putchar
+ *print_alphabet_x10 - prints alphabets using _putchar ten times
  *
 */
 void print_alphabet(void)
 {
 	int ch = 0;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
