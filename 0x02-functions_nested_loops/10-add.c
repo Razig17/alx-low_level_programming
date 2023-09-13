5a478bd4ea7d98313d06cae911d5968376cf7a02
Razig <razig@soleb.com>
2023-09-12 19:56:31 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - adds two numbers
+ * Return : returns the sum of two numebrs
+ *@num1 @num2 : the numbers that will be added
+ */
+#include "main.h"
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
