1c3c084a09eb8fddbbd945e43ff6c4096b320e27
Razig <razig@soleb.com>
2023-09-12 17:12:44 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - gives the absolute value of a number
+ *
+ *@n: checks input of fucnction
+ *
+ * Return: returns the last digit of a number
+*/
+int print_last_digit(int n)
+{
+	int ls;
+
+	if (a < 0)
+		ls = (-1) * (ls % 10);
+	ls = ls % 10;
+	
+	return (ls);
+}
