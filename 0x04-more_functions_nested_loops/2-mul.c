c5a0df47aae0de83ec2172d284064bc6f4b41e48
Razig <razig@soleb.com>
2023-09-14 16:17:19 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ *mul - multiplies two numbers
+ *@a :the frist number
+ *@b:second number
+ *Return:  returns the multiplies two numebrs
+ */
+#include "main.h"
+
+int mul(int a, int b)
+{
+	return (num1 * num2);
+}
