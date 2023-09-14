cab046d144884c2d8d6acbab582d330df9cf0c27
Razig <razig@soleb.com>
2023-09-14 16:32:12 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 using _putchar
+ * print_most_numbers - prints numbers from 0 to 9 using _putchar
  *			except 2 and 4
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 0;
 
