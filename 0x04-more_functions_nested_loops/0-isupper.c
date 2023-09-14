fe8138c0d7f0d8558d925d48abe2b8d0eb9bdad2
Razig <razig@soleb.com>
2023-09-14 16:01:35 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for lowercase character
+ *
+ *@c: checks input of fucnction
+ *
+ * Return: returns 1 if c is lowercse otherwise returns 0
+*/
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
