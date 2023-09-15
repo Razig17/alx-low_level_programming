81cb228107b0bf25746cd04517cda7778c841b6c
Razig <razig@soleb.com>
2023-09-14 17:52:31 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ *
+ *@i:  input of fucnction
+ *
+*/
+void print_line(int n)
+{
+	int n;
+
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
