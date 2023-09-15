e2c4537b4c0ceb84c01db1c2d38ad9b64c781a9d
Razig <razig@soleb.com>
2023-09-14 19:31:47 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-
+/**
+ *main - solves The “Fizz-Buzz test”
+ *
+ *Return: Always 0.
+ */
 int main (void)
-  {
-int i;
-for (i = 1; i < 100; i++)
 {
-    if ((i % 5 == 0) && (i % 3 != 0)){
-    printf("Buzz ");}
-    
-else if ((i % 3 == 0) && (i % 5 != 0))
-    printf("Fizz ");
-    
-else if ((i % 3 == 0) && (i % 5 != 0))
-printf("Fuzz ");
-    
- else if ((i % 3 == 0) && (i % 5 == 0))
-    printf("FizzBuzz ");
+	int i;
 
-    else
-    printf("%d " ,i);
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 5 == 0) && (i % 3 != 0)){
+		printf("Buzz ");}
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		printf("Fizz ");
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		printf("Fuzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		printf("FizzBuzz ");
+		else
+		printf("%d " ,i);
+	}
+	printf("Buzz\n");
+	return (0);
 }
- printf("Buzz\n");
-return (0);
-  }
