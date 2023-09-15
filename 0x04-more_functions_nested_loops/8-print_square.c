69b0ed58e0abef1aac310ef8b3ce6225b79e0225
Razig <razig@soleb.com>
2023-09-14 18:28:14 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,9 +9,12 @@ void print_square(int size)
 {
 	int k;
 
-	for ( k = size; k > 0; k--)
+	if (size <= 0)
+		_putchar('\n');
+	for (k = size; k > 0; k--)
 	{
 		int j = size;
+
 		while (j > 0)
 		{
 			_putchar('#');
