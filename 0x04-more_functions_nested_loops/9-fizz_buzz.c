ee4c39c0d04244ee09aff58ca72a13ed994d9af7
Razig <razig@soleb.com>
2023-09-14 19:34:35 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,14 +4,14 @@
  *
  *Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i < 100; i++)
 	{
-		if ((i % 5 == 0) && (i % 3 != 0)){
-		printf("Buzz ");}
+		if ((i % 5 == 0) && (i % 3 != 0))
+		printf("Buzz ");
 		else if ((i % 3 == 0) && (i % 5 != 0))
 		printf("Fizz ");
 		else if ((i % 3 == 0) && (i % 5 != 0))
@@ -19,7 +19,7 @@ int main (void)
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		printf("FizzBuzz ");
 		else
-		printf("%d " ,i);
+		printf("%d ", i);
 	}
 	printf("Buzz\n");
 	return (0);
