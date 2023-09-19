639fc40c143f68e6d74fc8a35921b9cd05dc89bc
Razig <razig@soleb.com>
2023-09-18 19:41:56 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 
 	for (i = 0; *(str + i) != '\0'; i++)
 		len += 1;
-	for (i = (len - 1) / 2 ; i < len ; i++)
+	for (i = len / 2 ; i < len ; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 }
