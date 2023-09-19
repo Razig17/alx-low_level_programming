b317b3eac0c0ed4ae9ceb313cce6a593b777dfe9
Razig <razig@soleb.com>
2023-09-18 20:06:38 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_array - prints a string, followed by a new line
+ *
+ *@a : the array
+ *@n: no of array elemnts
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1 )
+			prinf("%d, ", a);
+		else
+			 prinf("%d\n, a);
+	}
+}
