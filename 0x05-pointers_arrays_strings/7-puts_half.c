f53df8ef932fc661d8d8d4b7bbff69abe9d3d00d
Razig <razig@soleb.com>
2023-09-18 19:45:38 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+11;rgb:0000/0000/0000/**
  *puts_half - prints second half  of  a string, followed by a new line
  *
  *@str: the string
@@ -11,6 +11,10 @@ void puts_half(char *str)
 
 	for (i = 0; *(str + i) != '\0'; i++)
 		len += 1;
+	if ((len % 2) == 0)
+		len = (len - )1 / 2;
+	else
+		len = len / 2;
 	for (i = len / 2 ; i < len ; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
