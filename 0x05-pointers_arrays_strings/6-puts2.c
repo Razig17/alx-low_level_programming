5c19a738d745c751c72523f7c2592423320aef70
Razig <razig@soleb.com>
2023-09-18 19:29:09 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts2 - prints every other char of  a string, followed by a new line
+ *
+ *@str: the string
+ */
+void _puts2(char *str)
+{
+	int i;
+	int len = -1;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		len += 1
+	for (i = 0; i < len; i += 2)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
