be9e5259da72f2c5d747d0e4faf397591e93d77a
Razig <razig@soleb.com>
2023-09-21 14:51:23 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_strncat - a function that concatenates two strings.
+ *
+ *@dest: destination string
+ *@src: source string
+ *
+ *Return: the pointer to dest
+ *
+ */
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while ((j >= 0) && (j < n))
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
