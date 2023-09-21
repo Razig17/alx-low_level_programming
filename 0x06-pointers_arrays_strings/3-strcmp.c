50cbdb33a280f79efe2fbc92221d7b81b1f57af1
Razig <razig@soleb.com>
2023-09-21 16:55:09 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (*s1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			i = ((int)*s1) - ((int)*s1);
-			break;
-		}
 		s1++;
 		s2++;
 	}
-	return (i);
+	return (*s1 - *s2);
 }
