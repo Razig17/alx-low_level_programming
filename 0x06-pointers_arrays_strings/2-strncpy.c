7c75e512abc273c7a7669be4f974f2055d4ece2a
Razig <razig@soleb.com>
2023-09-21 15:06:26 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strspy - a function that concatenates two strings.
+ *_strncpy - a function that concatenates two strings.
  *
  *@dest: destination string
  *@src: source string
@@ -13,16 +13,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
-
-       
+	int j = 0;
 
 	while ((j >= 0) && (j < n))
 	{
-		*(dest + i) = *(src + j);
+		*(dest + j) = *(src + j);
 		if (*(src + j) == '\0')
 		break;
-		i++;
 		j++;
 		for ( ; i < n; i++)
 			dest[i] = '\0';
