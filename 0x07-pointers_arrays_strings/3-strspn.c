aa50b5c6130c53709fc0a1781d3f80982546e015
Razig <razig@soleb.com>
2023-09-26 15:12:16 -0700

    0x07 4th task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,9 +14,9 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 	unsigned int bytes = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] !=  '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
