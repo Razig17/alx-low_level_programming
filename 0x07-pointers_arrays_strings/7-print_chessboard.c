2e22fcd06f9b8d2cb194701ac2571ae3ab396076
Razig <razig@soleb.com>
2023-09-26 16:56:28 -0700

    0x07 7th task

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+0;276;0c/**
  *print_chessboard - a function that prints the chessboard
  *@a : the chess board
  */
@@ -13,7 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8 ; i++)
 	{
 		for (j = 0; j < 8 ; j++)
-			putchar(a[i][j]);
+			_putchar(a[i][j]);
 	}
 		putchar('\n');
 }
