f11dfc1171e95ba1aff50a5f4e3801b99573c0b9
Razig <razig@soleb.com>
2023-09-26 18:21:49 -0700

    0x07 8th task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,15 @@
  *
  */
 
-void print_diagsums(int *a, int size){
+void print_diagsums(int *a, int size)
+{
 	int i, j, sum1, sum2;
-	sum1 =0;
-	sum2 =0;
 
-	for (i = 0; i < size; i++ )
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
 		sum1 = sum1 + a[i * (size + 1)];
-	for (j = 1; j <= size; j++ )
+	for (j = 1; j <= size; j++)
 		sum2 = sum2 + a[j * (size - 1)];
 	printf("%d, %d\n", sum1, sum2);
 }
