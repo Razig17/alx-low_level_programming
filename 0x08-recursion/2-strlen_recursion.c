1c837cc55bdb9db921870b8dfba18d168b7bcced
Razig <razig@soleb.com>
2023-09-28 16:41:33 -0700

    0x08-recursion 3rd task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- *_strlen_recursion -a function that returns the length of a string
+ *_strlen_recursion - a function that returns the length of a string
  *
  *@s: pointer to an array
  *
+ *Return: returns the length of a string
  */
 
 int _strlen_recursion(char *s)
