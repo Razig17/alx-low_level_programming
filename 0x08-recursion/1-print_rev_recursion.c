09900fc912ba4d47e0351e89b047e9166d5d4a66
Razig <razig@soleb.com>
2023-09-28 16:31:14 -0700

    0x08-recursion 2nd task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
-/**
+11;rgb:0000/0000/0000/**
  *_print_rev_recursion - a function that prints a string, in reverse
  *
  *@s: pointer to an array
  *
  */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
