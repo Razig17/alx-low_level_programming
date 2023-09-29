51d8f8c5caa488d5e5e24151ef86d43a9b17ae5b
Razig <razig@soleb.com>
2023-09-28 19:03:01 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,15 +13,13 @@ int is_palindrome(char *s)
 	l = _strlen_recursion(s);
 	if (l <= 1)
 		return (1);
-	else if (l % 2 != 0)
-	        return (0);
 	else
 		return (is_pal(0, l, s));
 }
 
 int is_pal(int a , int z, char *s)
 {
-	if ((*(s + a) == *(s + z)) && (z > a))
+	if ((*(s + a) == *(s + z)) && (z >= a))
 		return (is_pal(a + 1, z - 1 , s));
 	else if (a > z)
 		return (1);
