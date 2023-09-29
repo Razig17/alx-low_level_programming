7834b9ccd2a7a6c2be235e40e8d5687200442b3a
Razig <razig@soleb.com>
2023-09-28 18:56:59 -0700

    0x08-recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int is_pal(int a , int z, char *s);
+int _strlen_recursion(char *s);
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = _strlen_recursion(char *s);
+	if (l <= 1)
+		return (1);
+	else if (z % 2 != 0)
+	        return (0);
+	else
+		return (is_pal(0, l, s));
+}
+
+int is_pal(int a , int z, char *s)
+{
+	if ((*(s + a) == *(s + z)) && (z > a))
+		return (is_pal(a + 1, z - 1 , s));
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *_strlen_recursion - a function that returns the length of a string
+ *
+ *@s: pointer to an array
+ *
+ *Return: returns the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
