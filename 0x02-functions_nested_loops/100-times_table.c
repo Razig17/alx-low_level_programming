b8bf46b83b83e2522171833a6988c9b91748fefd
Razig <razig@soleb.com>
2023-09-29 23:19:44 -0700

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 	else if (prod > 99)
 	{
 	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10) % 10 ) + '0');
+	_putchar(((prod / 10) % 10) + '0');
 	_putchar((prod % 10) + '0');
 	}
 	}
