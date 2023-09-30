c284dc0ead1a4bae5481d1cff163196a1321231e
Razig <razig@soleb.com>
2023-09-29 22:42:25 -0700

    0x02

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the n times table
+ *@n : the number
+ *
+*/
+
+void print_times_table(int n);
+{
+	int num, mult, prod;
+
+	if ((n > 15) || (n < 0))
+		return;
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+
+	for (mult = 1; mult <= 12; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+
+	prod = num * mult;
+
+	if (prod <= 99)
+	_putchar(' ');
+	else if (prod <= 9)
+		_putchar(' ');
+	else
+	_putchar((prod / 100) + '0');
+	_putchar((prod / 10) + '0');
+
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+
+}
+}
