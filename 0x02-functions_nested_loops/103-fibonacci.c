be8bf16583d218770a766e040141b41ddb0a1607
Razig <razig@soleb.com>
2023-09-30 00:09:03 -0700

    0x02

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+11;rgb:0000/0000/0000/**
  *main -prints the sum of the even-valued terms in Fibonacci numbers
  * but only the terms under 4,000,000
  *Return: Always 0
@@ -12,14 +12,13 @@ int main(void)
 	unsigned long int fib1 = 1;
 	unsigned long int fib2 = 2;
 
-	printf("%ld, %ld, ", fib1, fib2);
 	for (i = 2; fib2 < 4000000; i++)
 	{
 		temp = fib2;
 		fib2 = fib1 + fib2;
 		fib1 = temp;
 		if ((fib2 < 4000000) && (fib2 % 2 == 0))
-		        sum += fib2;
+			sum += fib2;
 	}
 	printf("%d\n", sum);
 	return (0);
