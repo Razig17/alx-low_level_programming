2eaee891889b1b46d7b87869f4402aaecdc1e5f5
Razig <razig@soleb.com>
2023-09-30 02:40:54 -0700

    0x02 && git push git add . && git commit -m 0x02

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,48 @@
 #include <stdio.h>
 /**
-11;rgb:0000/0000/0000*main -  prints the first 98 Fibonacci numbers
+ *main -  prints the first 98 Fibonacci numbers
  *
  *Return: Always 0
  */
 int main(void)
 {
 	int i;
-	unsigned long int temp;
+	unsigned long int temp, fib1_min , fib1_max, fib2_min, fib2_max;
 	unsigned long int fib1 = 1;
 	unsigned long int fib2 = 2;
+	unsigned long int half1, half2;
 
 	printf("%ld, %ld, ", fib1, fib2);
-	for (i = 3; i < 92; i++)
+	for (i = 3; i <= 92; i++)
 	{
 		temp = fib2;
 		fib2 = fib1 + fib2;
 		fib1 = temp;
-		if (i == 98)
-			printf("%lu\n", fib2);
-		else if (i < 98)
-			printf("%lu, ", fib2);
+	        printf("%lu, ", fib2);
+	}
+	fib1_max = fib1 / 10000000000;
+	fib2_max = fib2 / 10000000000;
+	fib1_min = fib1 % 10000000000;
+	fib2_min = fib2 % 10000000000;
+
+	for (i = 92; i <= 98; i++)
+	{
+		half1 = fib1_max + fib2_max;
+		half2 = fib1_min + fib2_min;
+		if (half2 > 9999999999)
+		{
+			half1++;
+			half2 %= 10000000000;
+		}
+		printf("%lu%lu", half1, half2);
+		if (i != 98)
+			printf(", ");
+		else if (i == 98)
+			printf("\n");
+		fib1_max = fib2_max;
+		fib1_min = fib2_min;
+		fib2_max = half1;
+		fib2_min = half2;
 	}
 	return (0);
 }
