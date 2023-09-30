bc20f00c66995fcaa2e674b807c1c60531f16c7a
Razig <razig@soleb.com>
2023-09-29 23:31:56 -0700

    0x02

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+0;276;0c
 /**
  *main - a program that computes and prints the sum of all the multiples
  of 3 or 5 below 1024
@@ -11,7 +11,7 @@ int main ()
 	int i;
 	int sum = 0;
 
-	for (i + 1; i < 1024; i++)
+	for (i = 3; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
