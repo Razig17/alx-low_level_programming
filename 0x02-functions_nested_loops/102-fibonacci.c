8159cabe862dac668b5e7d112f133a062909b73e
Razig <razig@soleb.com>
2023-09-29 23:56:18 -0700

    0x02

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,12 @@ int main(void)
 	unsigned long int fib2 = 2;
 
 	printf("%ld, %ld, ", fib1, fib2);
-	for (i = 2; i < 51; i++)
+	for (i = 2; i < 50; i++)
 	{
 		temp = fib2;
 		fib2 = fib1 + fib2;
 		fib1 = temp;
-		if (i == 50)
+		if (i == 49)
 			printf("%ld\n", fib2);
 		else if (i < 50)
 			printf("%ld, ", fib2);
