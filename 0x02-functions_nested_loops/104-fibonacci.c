a66d1da03ec680b46f85d35eaa92ab32902c3e79
Razig <razig@soleb.com>
2023-09-30 02:43:56 -0700

    0x02 && git push git add . && git commit -m 0x02

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 	int i;
-	unsigned long int temp, fib1_min , fib1_max, fib2_min, fib2_max;
+	unsigned long int temp, fib1_min, fib1_max, fib2_min, fib2_max;
 	unsigned long int fib1 = 1;
 	unsigned long int fib2 = 2;
 	unsigned long int half1, half2;
 
-	printf("%ld, %ld, ", fib1, fib2);
+	printf("%lu, %lu, ", fib1, fib2);
 	for (i = 3; i <= 92; i++)
 	{
 		temp = fib2;
 		fib2 = fib1 + fib2;
 		fib1 = temp;
-	        printf("%lu, ", fib2);
+	printf("%lu, ", fib2);
 	}
 	fib1_max = fib1 / 10000000000;
 	fib2_max = fib2 / 10000000000;
