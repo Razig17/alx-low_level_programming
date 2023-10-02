7e100a25233ff9b9f5975d67958cc69a510f72f7
Razig <razig@soleb.com>
2023-10-01 17:36:19 -0700

    last one

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,5 +16,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
+	if (*s2 == '\0')
+		return 0;
 	return (0);
 }
