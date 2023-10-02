d17e60ba72bfffdc7737d26f7b19d46adc8b433e
Razig <razig@soleb.com>
2023-10-02 16:27:26 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 /**
  * main - prints the sum of arguments passed into it
  *
@@ -20,21 +21,21 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (i = ; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		s = arg[i];
-		n = strlen(s);
-		for (j = 0; j < n; j++);
-		{
-			if (!isdigit(s[j]))
-	        	{
+	        s = argv[i];
+
+	        for (j = 0; j < strlen(s); j++)
+	        {
+			if (s[j] < 48 || s[j] > 57)
+		        {
 				printf("Error\n");
-				return (1);
+		        	return (1);
 			}
 		}
 		sum+= atoi(s);
 	}
 	printf("%d\n", sum);
-	return (0)
+	return (0);
 
 }
