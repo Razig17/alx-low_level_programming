d1ddb236ddedd0a64525e3c85292f51b6a43a98c
Razig <razig@soleb.com>
2023-10-02 18:55:42 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,33 +11,33 @@
 
 int main(int argc, char *argv[])
 {
-	int  n , sum = 0;
+	int  n, sum = 0;
 
-	if (argc != 2 )
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 
 	}
 
-	n = atoi(argv[1]);;
+	n = atoi(argv[1]);
 
 	if ((n / 25) != 0)
 	{
 		sum += n / 25;
 		n = n % 25;
 	}
-	 if ((n / 10 ) != 0)
+	if ((n / 10) != 0)
 	{
 		sum += n / 10;
 		n = n % 10;
 	}
-	 if ((n / 5) != 0)
+	if ((n / 5) != 0)
 	{
 		sum += n / 5;
 		n = n % 5;
 	}
-	 if ((n / 2) != 0)
+	if ((n / 2) != 0)
 	{
 		sum += n / 2;
 		n = n % 2;
