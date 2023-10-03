f2256f65aa698500108fa97752cdae4acd6b177d
Razig <razig@soleb.com>
2023-10-02 19:57:11 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - a function locates a character in a string.
+ *
+ *@s: a pointer to an array
+ *@c: a character
+ *Return: the pointer to first occurrence of the character
+ *in the string or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		return (s + i);
+	}
+	return ('\0');
+}
