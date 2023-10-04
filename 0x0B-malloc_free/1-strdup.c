9891283200c169591827121fbeaabb6aba5360ca
Razig <razig@soleb.com>
2023-10-04 16:29:06 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -33,9 +33,9 @@ char *_strdup(char *str)
 	if (str == 0)
 		return (NULL);
 	size = _strlen_recursion(str);
-	s = malloc(sizeof(char) * size);
-	 if (s == 0)
-		return (NULL); 
+	s = malloc(sizeof(char) * size + 1);
+	if (s == 0)
+		return (NULL);
 	i = 0;
 	while (i < size)
 	{
