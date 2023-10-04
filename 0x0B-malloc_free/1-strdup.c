e86e5a84d96d5db0b115ec2351512179d167baa0
Razig <razig@soleb.com>
2023-10-04 16:21:55 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strlen_recursion - a function that returns the length of a string
+ *
+ *@s: pointer to an array
+ *
+ *Return: returns the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *which contains a copy of the string given as a parameter.
+ *
+ *@str: a pointer to the string that will be copied.
+ *
+ * Return: NUll or pointer to the array.
+ */
+
+char *_strdup(char *str);
+{
+	char *s;
+	unsigned int i, size;
+
+	if (str == 0)
+		return (NULL);
+	size = _strlen_recursion(str);
+	s = malloc(sizeof(char) * size);
+	i = 0;
+	while (i < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
+}
