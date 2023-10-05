5879c7812a972212761c3870a0d5d5883aa375c7
Razig <razig@soleb.com>
2023-10-04 19:14:26 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,11 +16,13 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	mat = malloc(height * sizeof(int));
 	if (mat == NULL)
 	{
-		return (NULL);
 		free(mat);
+		return (NULL);
+
 	}
 
 	for (i = 0; i < height; i++)
