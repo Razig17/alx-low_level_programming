568f199f14ce230c748e3813bd4393bc37977815
Razig <razig@soleb.com>
2023-10-08 17:21:22 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	add = malloc(sizeof(int) * (max - min + 1);
+	add = malloc(sizeof(int) * (max - min + 1));
 	if (add == NULL)
 		return (NULL);
 
