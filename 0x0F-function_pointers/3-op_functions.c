8446b854dc116cf9e59e167ad4b10cc94218d50f
Razig <razig@soleb.com>
2023-10-11 19:59:07 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "calc.h"
+#include <stdio.h>
  /**
  * op_add - adds two numbers
  *
@@ -50,7 +51,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
+	{
+		printf("Error\n");
 		exit(100);
+	}
 	return (a / b);
 }
 
@@ -65,6 +69,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
+	{
+		printf("Error\n");
 		exit(100);
-	return (a % b);
+        }
+		return (a % b);
 }
