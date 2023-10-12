49bffb1c038ba4188a6a9be71d11d6932370ca87
Razig <razig@soleb.com>
2023-10-11 18:38:19 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *int_index - searches for an integer
+ *
+ *@array: a pointer to array
+ *@size: size of array
+ *@cmp: a function poinetr
+ *Return: Nothing.
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+	int n;
+
+	if (array != NULL && size > 0 && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
+}
