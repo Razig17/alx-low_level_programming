dcc755a1a2630c281591c95506d86fcab1b67d95
Razig <razig@soleb.com>
2023-10-11 17:43:25 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndf FUNC_POI_H
+#ifndef FUNC_POI_H
 #define FUNC_POI_H
 
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
