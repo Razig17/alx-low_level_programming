f4b5de37681bfc578bdbbe0a56e7b80280fac7f4
Razig <razig@soleb.com>
2023-10-12 15:47:34 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start(num, i);
+	va_start(num, n);
 	for (i = 0; i < n;i++)
 		sum += va_arg(num, int);
 	va_ends(num);
