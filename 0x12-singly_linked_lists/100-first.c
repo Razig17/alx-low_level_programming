a112ac8ab1af0e10a7b132a9da12d1ac69153233
Razig17 <razog1999@gmail.com>
2023-10-19 19:24:32 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void before_main(void) __attribute__ ((constructor));
+
+/**
+  * before_main - prints a string before main is executed
+  *
+  * Return: nothing.
+  */
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
