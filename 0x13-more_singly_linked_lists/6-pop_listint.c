09683776f6da8e0031127786b10318612621782c
Razig17 <razog1999@gmail.com>
2023-10-23 22:03:39 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,10 +14,10 @@ int pop_listint(listint_t **head)
 	listint_t *ptr ;
 	int data = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
-	ptr = *head->next;
-	data = *head->n;
+	ptr = (*head)->next;
+	data = (*head)->n;
 	free(*head);
 	*head = ptr;
 	return (data);
