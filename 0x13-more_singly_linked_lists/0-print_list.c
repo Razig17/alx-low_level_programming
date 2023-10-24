58d5ba41eadfcce592be506a840e14bb02d7cd51
Razig <razog1999@gmail.com>
2023-10-24 03:41:35 +0300

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a list of integers
+ *
+ * @h: a pointer to the list header
+ * Return: number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+			printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+
+}
