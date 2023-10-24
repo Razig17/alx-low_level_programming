c505342452030d9a3e7d10b90cae83f39a91e82a
Razig17 <razog1999@gmail.com>
2023-10-23 21:03:27 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (head* != NULL)
+	while (*head != NULL)
 	{
 		tmp = *head;
-		*head = *head->next;
+		*head = (*head)->next;
 		free(tmp);
 	}
 	free(*head);
