bb3707a0613468fcf9b468614877fdd624267062
Razig17 <razog1999@gmail.com>
2023-10-23 20:59:22 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: a pointer to the list header
+ * Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head* != NULL)
+	{
+		tmp = *head;
+		*head = *head->next;
+		free(tmp);
+	}
+	free(*head);
+	head = NULL;
+}
