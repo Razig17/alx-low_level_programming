2cb183bcecd0ed657c8593a18349ee13273fa5be
Razig17 <razog1999@gmail.com>
2023-12-14 16:54:28 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
   * Return: 1 if it worked or -1 otherwise
   */
 
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
