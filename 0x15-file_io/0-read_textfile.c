df08070af18b6e46bff5aae7ee5f873a70e16052
Razig17 <razog1999@gmail.com>
2023-12-18 17:35:30 -0800

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters
+ *
+ * Return: returns the actual number of letters it could read and print
+ * or 0 if fail can't be opend or red or file name is NULL or write fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char *buffer;
+
+	if (filename == NULL || letters == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	bytes = read(fd, buffer, letters);
+	bytes = write(STDOUT_FILENO, buffer, bytes);
+
+	free(buffer);
+	close(o);
+
+	return (bytes);
+}
