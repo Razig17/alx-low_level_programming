8c3ec4d8bb171b80f0da788ca8e85d0bb492597d
Razig17 <razog1999@gmail.com>
2023-12-18 18:09:58 -0800

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	fd2 = write(o, text_content, len);
-	if (o == -1 || w == -1)
+	fd2 = write(fd, text_content, len);
+	if (fd == -1 || fd2 == -1)
 		return (-1);
 	close(fd);
 
