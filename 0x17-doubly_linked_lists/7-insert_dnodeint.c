3542e4b7a643f199226296daed31fc176f83c26a
Razig17 <razog1999@gmail.com>
2024-01-26 04:17:37 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	tmp = *h;
+	tmp = (*h);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
