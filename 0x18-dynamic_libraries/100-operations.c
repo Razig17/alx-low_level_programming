9865e463fe232dedb40c70537107ee5721920538
Razig17 <razog1999@gmail.com>
2024-01-30 02:27:12 +0300

    task 3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+
+/**
+ * add - add 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculate the modules of the division between 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
