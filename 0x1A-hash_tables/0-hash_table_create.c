63ec464b6a095c9bece1460d2a40b172e4a137fd
Razig17 <razog1999@gmail.com>
2024-02-17 00:34:28 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Write a function that creates a hash table.
+ * hash_table_create - A function that creates a hash table.
  *
- * @size: the size of hash table
+ * @size: The size of hash table
  *
- * Return: a pointer to the hash table or NULL if failed
+ * Return: A pointer to the hash table or NULL if failed
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
